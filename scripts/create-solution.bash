a2e650d0ab4c6c9279f392bb8394d35a7aae384e
Tim Davis <contact@timdav.is>
2020-02-07 13:13:47 -0800

    Update solution creation script to account for double runs.

--- scripts/create-solution.bash
+++ scripts/create-solution.bash
@@ -5,14 +5,19 @@ if [ "$#" -eq 0 ]; then
   exit 1
 fi
 
-if [ ! -d "exercises/$1" ]; then
-  echo "Error: Exercise \"$1\" does not exist." 1>&2
-  exit 1
-fi
-
 exercise_name="$1"
 exercise_path="exercises/$exercise_name"
 solution_path="solutions/$exercise_name"
+
+if [ ! -d $exercise_path ]; then
+  echo "Error: Exercise \"$exercise_name\" does not exist." 1>&2
+  exit 1
+fi
+
+if [ -d $solution_path ]; then
+  echo "Error: Solution \"$exercise_name\" already exists." 1>&2
+  exit 1
+fi
 
 cp -Rp $exercise_path $solution_path
 mkdir -p $solution_path
